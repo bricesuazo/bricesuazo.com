cdb829e00e82572c30a93d346d887ccc760c7d37
Brice Suazo <bricesuazo@gmail.com>
2023-10-28 12:23:51 +0800

    test

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -6,7 +6,12 @@ import "@bricesuazo/auth/env.mjs";
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@bricesuazo/api", "@bricesuazo/auth", "@bricesuazo/db"],
+  transpilePackages: [
+    "@bricesuazo/api",
+    "@bricesuazo/auth",
+    "@bricesuazo/constant",
+    "@bricesuazo/db",
+  ],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,16 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
 import "~/styles/globals.css";
 
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { headers } from "next/headers";
+import { Analytics } from "@vercel/analytics/react";
+
+import { meta } from "@bricesuazo/constant";
 
 import { TRPCReactProvider } from "./providers";
-
-const fontSans = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 /**
  * Since we're passing `headers()` to the `TRPCReactProvider` we need to
@@ -19,27 +16,54 @@ const fontSans = Inter({
  */
 export const dynamic = "force-dynamic";
 
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export const metadata: Metadata = {
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
+  metadataBase: new URL(meta.url),
+  title: {
+    default: meta.title,
+    template: `%s | ${meta.title}`,
+  },
+  description: meta.description,
   openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    title: meta.title,
+    description: meta.description,
+    url: meta.url,
+    siteName: meta.title,
+    locale: meta.locale,
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
   twitter: {
+    title: meta.title,
+    description: meta.description,
+    creator: meta.accounts.twitter.username,
     card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
+  },
+  icons: {
+    shortcut: "/favicon.ico",
   },
 };
 
-export default function Layout(props: { children: React.ReactNode }) {
+export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={["font-sans", fontSans.variable].join(" ")}>
         <TRPCReactProvider headers={headers()}>
+          <Analytics />
           {props.children}
         </TRPCReactProvider>
       </body>
