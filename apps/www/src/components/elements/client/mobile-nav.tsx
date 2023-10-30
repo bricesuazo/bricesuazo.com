600334009abdbd22b624dff97d6d9cc34db5e6b7
Brice Suazo <bricesuazo@gmail.com>
2023-10-30 17:53:19 +0800

    Add sheet for mobile nav

--- apps/www/src/components/elements/client/mobile-nav.tsx
+++ apps/www/src/components/elements/client/mobile-nav.tsx
@@ -5,70 +5,72 @@ import Link from "next/link";
 import { AlignLeftIcon, XIcon } from "lucide-react";
 
 import { nav } from "@bricesuazo/constant/config";
+import { Button } from "@bricesuazo/ui/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@bricesuazo/ui/ui/sheet";
 import { cn } from "@bricesuazo/ui/utils";
 
 export default function MobileNav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuItems = [...nav.left, ...nav.right];
+  const menuItems: { href: string; title: string; target?: string }[] = [
+    ...nav.left,
+    ...nav.right,
+  ];
 
   return (
     <>
-      <button
-        className="burger visible relative h-10 w-10 border-0 bg-transparent md:hidden"
-        aria-label="Toggle menu"
-        type="button"
-        onClick={() => {
-          isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true);
-        }}
-      >
-        {!isMenuOpen ? (
-          <AlignLeftIcon className="absolute left-1/2 top-1/2 h-5 w-5 -translate-x-1/2 -translate-y-1/2 scale-100 text-gray-900 !opacity-100 duration-200 motion-reduce:transition-none dark:text-gray-100" />
-        ) : (
-          <XIcon className="absolute left-1/2 top-1/2 h-5 w-5 -translate-x-1/2 -translate-y-1/2 scale-100 text-gray-900 !opacity-100 duration-200 motion-reduce:transition-none dark:text-gray-100" />
-        )}
-      </button>
-      <div
-        className={cn(
-          {
-            "rendered pointer-events-all !opacity-100": isMenuOpen,
-            "pointer-events-none": !isMenuOpen,
-          },
-          "absolute left-0 top-0 z-[1001] mt-[73px] flex h-screen w-3/4 flex-col opacity-0 backdrop-blur-xl duration-200 motion-reduce:transition-none md:hidden",
-        )}
-      >
-        <div className="firefox:bg-opacity-100 dark:firefox:bg-opacity-100 h-full border-r-[1px] border-black/[10%] bg-white bg-opacity-70 px-3.5 shadow duration-200 motion-reduce:transition-none dark:border-neutral-800 dark:bg-[#161617] dark:bg-opacity-70 dark:shadow-2xl">
-          <div className="mt-3">
+      <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+        <SheetTrigger className="md:hidden" asChild>
+          <Button size="icon" variant="outline">
+            {!isMenuOpen ? (
+              <AlignLeftIcon size="1rem" />
+            ) : (
+              <XIcon size="1rem" />
+            )}
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle className="text-left">Brice Suazo</SheetTitle>
+            <SheetDescription className="text-left">
+              Navigation Links
+            </SheetDescription>
+          </SheetHeader>
+
+          <div className="mt-4">
             {menuItems.map((item, index) => {
               return (
-                <p
+                <Link
+                  href={item.href}
                   key={index}
+                  target={item.target}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
                   className={cn(
                     {
                       "w-0 translate-x-[-16px] border-transparent opacity-0 dark:border-transparent":
                         !isMenuOpen,
-                      "w-full translate-x-0 border-gray-200 opacity-100 dark:border-neutral-800":
+                      "w-full translate-x-0 border-muted opacity-100":
                         isMenuOpen,
                     },
-                    "group whitespace-nowrap border-b text-sm font-semibold text-gray-900 duration-200 motion-reduce:transition-none dark:text-gray-100",
+                    "group whitespace-nowrap border-b text-sm font-semibold duration-200 motion-reduce:transition-none",
                   )}
                   style={{ transitionDelay: `${150 * index - 50}ms` }}
                 >
-                  <Link
-                    href={item.href}
-                    key={index}
-                    className="flex w-auto p-4 pb-4 duration-200 group-hover:pl-6 motion-reduce:transition-none"
-                    onClick={() => {
-                      isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true);
-                    }}
-                  >
+                  <p className="flex w-auto p-4 pb-4 text-muted-foreground duration-200 group-hover:pl-6 group-hover:text-foreground motion-reduce:transition-none">
                     {item.title}
-                  </Link>
-                </p>
+                  </p>
+                </Link>
               );
             })}
           </div>
-        </div>
-      </div>
+        </SheetContent>
+      </Sheet>
     </>
   );
 }
