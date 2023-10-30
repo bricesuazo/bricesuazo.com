c11199662d6c5e3d8ffa492c5f0148783aa8a3c4
Brice Suazo <bricesuazo@gmail.com>
2023-10-30 16:58:13 +0800

    Fix format

--- apps/link/next.config.mjs
+++ apps/link/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  transpilePackages: [
-    "@bricesuazo/constant",
-  ],
+  transpilePackages: ["@bricesuazo/constant"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
